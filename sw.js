870e5ae6211fa15f074472704415fad56bd2fda2
aleksaabesa <aleksandreabesadze612@gmail.com>
2026-01-30 11:58:19 +0400

    Add service worker configuration for app

---
+++ sw.js
@@ -0,0 +1,15 @@
+{
+  "short_name": "გაჩითე",
+  "name": "გაჩითე Live",
+  "icons": [
+    {
+      "src": "შენი_სურათის_ლინკი.png", 
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#000",
+  "theme_color": "#ff0000"
+}
